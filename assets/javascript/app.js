7a0760fad8e67311e8f6529a66b09f98077d5d06
Rebecca Palmore <rebeccapalmore@Rebeccas-MacBook-Pro.local>
2017-03-12 08:22:01 -0500

    updating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,8 +23,10 @@ var q5Choices = ["Q5 choice 1", "Q5 choice 2", "Q5 choice 3", "Q5 choice 4",]
 // var correctChoices = ["Q1 choice 1", "Q2 choice 2", "Q3 choice 1", "Q4 choice 1", 
 // "Q5 choice 1"]
 
-var correctChoices = (q1Choices[0], q2Choices[0], q3Choices[0], q4Choices[0], 
-q5Choices[0]);
+var correctChoices = [q1Choices[0], q2Choices[0], q3Choices[0], q4Choices[0], 
+q5Choices[0]];
+console.log(correctChoices);
+console.log(correctChoices[0]);
 
 var userGuess = [];
 
@@ -33,29 +35,88 @@ var userGuess = [];
 // }
 // var interval;
 
-
 /* Step one: Attach a click event to start button
 When user clicks "start," the following will happen:
 	Start button will be hidden/replaced with
 		1. Question 1 x
 		2. 4 buttons showing possibile choices x
 		3. Timer starts x
-
 */
 
+// function startGame () {
+// $("#startButton").click(function() {
+// 	$(".start").remove();
+// 	runTimer();
+// 	$("#timer").text("Time remaining:" + (" ") + timer + (" ") + "seconds");
+// 	$("#question").text("Question 1");
+// 	for (var i = 0; i < q1Choices.length; i++) {
+// 		var userChoices = $("<button>");
+// 		userChoices.addClass("choice-button button button-color");
+// 		userChoices.attr("data-button", q1Choices[i]);
+// 		userChoices.text(q1Choices[i]);
+// 		$("#buttons").append(userChoices);
+// 		console.log(q1Choices[i]);
+// 	}
+// })
+// };
+
+// $("#buttons").on("click", function (){
+// 	console.log("i was clicked");
+// if (userGuess == correctChoices[0]) {
+// 	console.log("You win!");
+// 	console.log(userGuess);
+// }
+// });
+
+// THIS IS ALSO WORKING CODE
 function startGame () {
 $("#startButton").click(function() {
 	$(".start").remove();
-	run();
+	runTimer();
 	$("#timer").text("Time remaining:" + (" ") + timer + (" ") + "seconds");
 	$("#question").text("Question 1");
-	for (var i = 0; i < 4; i++) {
-	$("<p><button>" + q1Choices[i] + "</button></p>").appendTo(".choices");
-		}
-	})
+	for (var i = 0; i < q1Choices.length; i++) {
+	$("<button>" + q1Choices[i] + "</button>").appendTo(".choices");
+	console.log(q1Choices[i]);
+	$( "button" ).eq( i ).on( "click", { value: i }, function( event ) {
+    console.log("button = " + $(this).index());
+    // if ($(this).index() == q1Choices.index) {
+    // 	console.log("Yeah!!!!");
+    }
+})
+}
+})
 };
 
-// Step two: Stop timer when user clicks on any "choices" button x
+// $(".choices, #buttons").on("click", function(){
+
+// 	console.log("I was clicked!");
+// })
+/* Step two: Capture the click event data
+*/
+
+/* Step three: Dynamically create new div and append to "correctChoice" 
+if user picks correctly
+*/
+// var winningAnswer = $("<div>");
+// winningAnswer.addClass("test")
+// var losingAnswer = $("<div>");
+// winningAnswer.addClass("test")
+// // if user selects winning answer, do this:
+
+// winningAnswer.text("You win!");
+// $(".correctChoice").append(winningAnswer);
+// // else, do this:
+
+// winningAnswer.text("You lose!");
+// $(".correctChoice").append(losingAnswer);
+// console.log($(this));
+// console.log(this);
+// console.log(correctChoices[0]);
+// console.log(q1Choices[0]);
+// });
+
+// Step four: Stop timer when user clicks on any "choices" button x
 
 // Set timer to 30 seconds
 var timer = 30;
@@ -64,7 +125,7 @@ var intervalID;
 // click event
 $(".choices").on("click", stop);
 // The "run" function sets an interval and decrements counter by one per second
-function run() {
+function runTimer() {
 	intervalID = setInterval(decrement, 1000);
 }
 // The decrement function
@@ -81,33 +142,8 @@ function stop() {
 	clearInterval(intervalID);
 }
 
-// Step three: Dynamically create new div and append to "correctChoice" if user picks correctly
-
-$(".choices").click(function() {
-$(".choices, #question").remove();
-var winningAnswer = $("<div>");
-winningAnswer.addClass("test")
-var losingAnswer = $("<div>");
-winningAnswer.addClass("test")
-// if user selects winning answer, do this:
-
-winningAnswer.text("You win!");
-$(".correctChoice").append(winningAnswer);
-// else, do this:
-
-winningAnswer.text("You lose!");
-$(".correctChoice").append(losingAnswer);
-console.log($(this));
-console.log(this);
-console.log(correctChoices[0]);
-console.log(q1Choices[0]);
-});
 
 startGame();
-
-
-
-
 
 
 
