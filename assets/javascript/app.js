2155cc708d48a093bb2f318d976b91e19805d052
rebecca palmore <rpalmore@wttw.com>
2017-03-12 18:33:55 -0500

    updating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,12 +23,13 @@ var q5Choices = ["Q5 choice 1", "Q5 choice 2", "Q5 choice 3", "Q5 choice 4",]
 var correctChoices = ["Q1 choice 1", "Q2 choice 1", "Q3 choice 1", "Q4 choice 1", 
 "Q5 choice 1"]
 
+var solutions = ["In fact, Q1 choice 1", "In fact, Q2 choice 1", "In fact, Q3 choice 1",
+"In fact, Q4 choice 1", "In fact, Q5 choice 1"]
+
 // var correctChoices = [q1Choices[0], q2Choices[0], q3Choices[0], q4Choices[0], 
 // q5Choices[0]];
 console.log(correctChoices);
 // console.log(correctChoices[0]);
-
-var userGuess = [];
 
 /* 
 When user clicks "start," do the following:
@@ -45,12 +46,12 @@ When user clicks "start," do the following:
 // ADD TO WORKING CODE
 
 var winningAnswer = $("<div>");
-winningAnswer.addClass("test")
-winningAnswer.text("You win!");
+winningAnswer.addClass("test");
+winningAnswer.text("This will be winning image");
 
 var losingAnswer = $("<div>");
-losingAnswer.addClass("test")
-losingAnswer.text("You lose!");
+losingAnswer.addClass("test");
+losingAnswer.text("This will be winning image");
 
 var winCounter = 0;
 var lossCounter = 0;
@@ -74,15 +75,20 @@ var unanswered = 0;
 function play() {
 	$("#startButton").on("click", function() {
 	$(".startButton").remove();
-	runTimer();
-	$("#timer").text("Time remaining:" + (" ") + timer + (" ") + "seconds");
-	$("#question").text(questions[0]);
+	startTimer();
+	// $("#question").text(questions[0]);
+	revealQuestion();
 	createButtons();
 	compareClickValues();
   })
 }
 
 play();
+
+function revealQuestion () {
+	// for (var i = 0; i < questions.length; i++)
+	$("#question").text(questions[0]);
+}
 
 function createButtons() {
 	for (var i = 0; i < q1Choices.length; i++)
@@ -100,41 +106,20 @@ function compareClickValues() {
 		$(".correctChoice").append(winningAnswer);
 		winCounter++;
 		console.log("this is win counter " + winCounter);
+		setTimeout(startTimer, 1000 * 3);
+		setTimeout(revealQuestion, 1000 * 3);
 	} else {
 		$("#question").text("Nope!");
-		// add correct answer to div somewhere
+		$("#solution").text(solutions[0]);
 		$(".choices").remove();
-		$(".incorrectChoice").append(losingAnswer);
+		$(".correctChoice").append(winningAnswer);
 		lossCounter++;
 		console.log("this is loss counter " + lossCounter);
+		setTimeout(startTimer, 1000 * 3);
 	}
   })
 }
 
-// function displayResult () {
-// 	setTimeout(function () {
-// 		$(".play").hide(); }, 3000);
-// 	}
-
-
-
-// function reset () {
-// 	$(".play").show(); 
-// }
-
-// function advance () {
-// 	for (i = 0; i < questions.length; i++){
-// 		$("#question").text(questions[i]);
-// 	}
-// }
-
-// Need advance/reset function
-// function resetGame () {
-// 	$("#timer").text("Time remaining:" + (" ") + timer + (" ") + "seconds");
-// 	$("#question").text("Question 1");
-// 	createButtons();
-// 	compareClickValues();
-// }
 
 // Step four: Stop timer when user clicks on any "choices" button x
 
@@ -143,33 +128,34 @@ var timer = 20;
 // Variable to hold interval ID
 var intervalID;
 
-// The "run" function sets an interval and decrements counter by 1 per second
-function runTimer() {
+// The start function sets the clock and decrements by 1 per second
+function startTimer(){
+	timer = 20;
+	$("#timer").text("Time remaining:" + (" ") + timer + (" ") + "seconds");
 	intervalID = setInterval(decrement, 1000);
 }
-// The decrement function
+
+// Decrement function
 function decrement () {
 	timer--;
+
 // Replace timer with this one that is counting down
 	$("#timer").text("Time remaining:" + (" ") + timer + (" ") + "seconds");
 	if (timer === 0) {
 		stop();
 		console.log("Time's up!!!")
 		$("#question").text("Time's up!!!");
-		// add correct answer to div somewhere
+		$("#solution").text(solutions[0]);
 		$(".choices").remove();
-		$(".incorrectChoice").append(losingAnswer);
+		$(".correctChoice").append(winningAnswer);
 		unanswered++;
-		// displayResult();
+		setTimeout(startTimer, 1000 * 3);
 	}
 }
-// stop function
+// The stop timer function
 function stop() {
 	clearInterval(intervalID);
 }
-
-
-// startGame();
 
 
 
