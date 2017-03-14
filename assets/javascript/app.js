cf988b85a89e8df77dc5c0b5c7d040b7d6eaf2da
rebecca palmore <rpalmore@wttw.com>
2017-03-14 00:48:07 -0500

    updating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,59 @@
 $( document ).ready(function() {
-	console.log( "ready!" );
 });
 
-var questions = ["This is question 1", "Question 2", "Question 3", "Question 4", "This is question 5"]
+// Here are all the lengthy quotes and choices.
+var questions = [
+'"One of the key problems today is that politics is such a disgrace.'
++ ' Good people don’t go into government."', 
+'"A bachelor is a guy who never made the same mistake once."', 
+'"Confidence is 10 percent hard work and 90 percent delusion."', 
+'"There is no terror in the bang, only in the anticipation of it."', 
+'"What do you want a meaning for? Life is a desire, not a meaning."'
+]
 
-var q1Choices = ["Q1 choice 1", "Q1 choice 2", "Q1 choice 3", "Q1 choice 4",]
-var q2Choices = ["Q2 choice 1", "Q2 choice 2", "Q2 choice 3", "Q2 choice 4",]
-var q3Choices = ["Q3 choice 1", "Q3 choice 2", "Q3 choice 3", "Q3 choice 4",]
-var q4Choices = ["Q4 choice 1", "Q4 choice 2", "Q4 choice 3", "Q4 choice 4",]
-var q5Choices = ["Q5 choice 1", "Q5 choice 2", "Q5 choice 3", "Q5 choice 4",]
+var q1Choices = [
+'Bill Maher',
+'Donald Trump',
+'Oprah Winfrey',
+'Ralph Nader'
+]
+var q2Choices = [
+'Phyllis Diller',
+'Jerry Seinfeld',
+'Johnny Carson',
+'Wanda Sykes'
+]
+var q3Choices = [
+'Woody Allen',
+'Dolly Parton',
+'Mark Twain',
+'Tina Fey'
+]
+var q4Choices = [
+'Orson Welles',
+'Alfred Hitchcock', 
+'M. Night Shyamalan',
+'Stephen King'
+]
+var q5Choices = [
+'Steve Jobs',
+'Maya Angelou',
+'Confucius',
+'Charlie Chaplin'
+]
 
-var solutions = ["The correct answer was: Q1 choice 1", "In fact, Q2 choice 1", "In fact, Q3 choice 1",
-"In fact, Q4 choice 1", "In fact, Q5 choice 1"]
+// The solutions for each question, which we'll display.
+var solutions = [
+'The 45th president of the United States called politics "a disgrace."',
+'Phyllis Diller made her debut as a stand-up comedian at age 37.', 
+'The former "SNL" star who got her start at The Second City'
++ ' once called Evanston home.',
+'The late "Master of Suspense" was a pioneer in the genres of'
++ ' suspense and psychological thrillers.',
+'The silent film icon was best known for his character "the tramp."'
+]
 
-/* 
-When user clicks "start," do the following:
-	1. Remove start button
-	2. Display decrementing timer
-	3. Display question 1
-	4. Dynamically create 4 buttons showing choice options
-	5. On button click, store value of button index
-	6. Stop timer
-	7. Declare index of winning choice
-	8. Compare index of selected button with index of correct choice
-*/
-
+// IMAGE ELEMENT VARIABLES TO USE IN CODE BELOW	
 var giphy = $("<img>");
 giphy.attr("src", "assets/images/giphy.gif");
 
@@ -35,57 +64,44 @@ var winningAnswer4 = $("<img>");
 var winningAnswer5 = $("<img>");
 
 winningAnswer.attr("src", "assets/images/Trump.jpg");
-winningAnswer2.attr("src", "assets/images/Chaplin.jpg");
-winningAnswer3.attr("src", "assets/images/Diller.jpg");
+winningAnswer2.attr("src", "assets/images/Diller.jpg");
+winningAnswer3.attr("src", "assets/images/TinaFey.jpg");
 winningAnswer4.attr("src", "assets/images/Hitchcock.jpg");
-winningAnswer5.attr("src", "assets/images/TinaFey.jpg");
+winningAnswer5.attr("src", "assets/images/Chaplin.jpg");
 
-// Adding in button hover elements
-$("button").hover(function(){
-    $(this).css("background-color", "#fafb10");
-    }, function(){
-    $(this).css("background-color", "white");
- });
-
- // And some fun link hover decoration
-$("a").hover(function(){
-    $(this).css("background-color", "#18c8e4");
-     }, function(){
-    $(this).css("background-color", "white");
- });
-
-
-// var photos = ["assets/images/Trump.jpg", "assets/images/Chaplin.jpg"]
-// for (i = 0; i < photos.length; i++) {
-// 	console.log(photos[i]);
-// 	winningAnswer.attr("src", photos[i]);
-// }
-
+// Assign counters for scores
 var winCounter = 0;
 var lossCounter = 0;
 var unanswered = 0;
 
+// START FUNCTION
 function start() {
+ 	winCounter = 0;
+	lossCounter = 0;
+	unanswered = 0;
 	$("#giphy").prepend(giphy);
 	$("#startButton").on("click", function() {
 	$(".startButton").remove();
-	$("#giphy").fadeOut();
+	$("#giphy").remove();
 	roundOne();
   })
 }
 start();
 
 
-// WHAT IF WE DO A BIG FOR LOOP AROUND EVERYTHING AND USE I???
+/* WE ARE WORKING THROUGH GAME IN ROUNDS
+I wish I knew how to do this more efficiently!
+*/
 function roundOne() {
 	startTimer();
 	$("#question").text(questions[0]);
 	for (var i = 0; i < q1Choices.length; i++) {
 	$("<button>" + q1Choices[i] + "</button>").appendTo(".choices");
-	}
+	}	
 	compareClickValues();
 };
 
+// Each round will call several functions and create buttons
 function roundTwo () {
 	startTimer();
 	$("#question").text(questions[1])
@@ -95,6 +111,7 @@ function roundTwo () {
 	compareClickValues2();
 };
 
+// I know there's a better way to do this! 
 function roundThree () {
 	startTimer();
 	$("#question").text(questions[2])
@@ -122,156 +139,167 @@ function roundFive () {
 	compareClickValues5();
 };
 
-function displayScore () {
-	// startTimer();
-	$("#question").text(questions[4])
-	// for (var i = 0; i < q4Choices.length; i++) {
-	// $("<button>" + q5Choices[i] + "</button>").appendTo(".choices");
-	}
-	// compareClickValues5();
-// };
-// TRY AND WRITE ONE FUNCTION FOR ALL OF THIS
+// HERE IS THE CODE TO CHECK VALUES, DELIVER OUTCOMES
 function compareClickValues() {
-$("button").on("click", function() {
+  $("button").on("click", function() {
 	stop ();
-	if ($(this).index() == 0) {
-		$("#question").text("Correct!");
-		$("button").hide();
-		$("#question").append(winningAnswer);
-		winCounter++;
-		setTimeout(roundTwo, 1000 * 3);
-	} else {
-		$("#question").text("Nope!");
-		$("button").hide();
-		$("<p>" + solutions[1] + "</p>").appendTo("#question");
-		$("#question").append(winningAnswer);
-		lossCounter++;
-		setTimeout(roundTwo, 1000 * 3);
-	}
-  })
+	$("button").hide();
+	setTimeout(roundTwo, 1000 * 3);
+		// compare user choice with correct answer, run code
+		if ($(this).index() == 1) {
+			$("#question").text("Correct!");
+			$("<p>" + solutions[0] + "</p>").appendTo("#question");
+			$("#question").append(winningAnswer);
+			winCounter++;
+		} else {
+			$("#question").text("Nope!");
+			$("<p>" + solutions[0] + "</p>").appendTo("#question");
+			$("#question").append(winningAnswer);
+			lossCounter++;
+		}
+  	})
 }
-
-// NEW WORKING CODE
+// We are moving through rounds here too ... too much code!!!
 function compareClickValues2() {
-$("button").on("click", function() {
+  $("button").on("click", function() {
 	stop ();
-	if ($(this).index() == 4) {
-		$("#question").text("Correct!");
-		$("button").hide();
-		$("#question").append(winningAnswer2);
-		winCounter++;
-		setTimeout(roundThree, 1000 * 3);
-	} else {
-		$("#question").text("Nope!");
-		$("button").hide();
-		$("<p>" + solutions[1] + "</p>").appendTo("#question");
-		$("#question").append(winningAnswer2);
-		lossCounter++;
-		setTimeout(roundThree, 1000 * 3);
-	}
-  })
+	$("button").hide();
+	setTimeout(roundThree, 1000 * 3);
+		if ($(this).index() == 4) {
+			$("#question").text("Correct!");
+			$("<p>" + solutions[1] + "</p>").appendTo("#question");
+			$("#question").append(winningAnswer2);
+			winCounter++;
+		} else {
+			$("#question").text("Nope!");
+			$("<p>" + solutions[1] + "</p>").appendTo("#question");
+			$("#question").append(winningAnswer2);
+			lossCounter++;
+		}
+	})
 }
 
-// NEW WORKING CODE
+// ROUND THREE
 function compareClickValues3() {
-$("button").on("click", function() {
+  $("button").on("click", function() {
 	stop ();
-	if ($(this).index() == 8) {
-		$("#question").text("Correct!");
-		$("button").hide();
-		$("#question").append(winningAnswer3);
-		winCounter++;
-		setTimeout(roundFour, 1000 * 3);
-	} else {
-		$("#question").text("Nope!");
-		$("button").hide();
-		$("<p>" + solutions[2] + "</p>").appendTo("#question");
-		$("#question").append(winningAnswer3);
-		lossCounter++;
-		setTimeout(roundFour, 1000 * 3);
-	}
-  })
+	$("button").hide();
+	setTimeout(roundFour, 1000 * 3);
+		if ($(this).index() == 11) {
+			$("#question").text("Correct!");
+			$("<p>" + solutions[2] + "</p>").appendTo("#question");
+			$("#question").append(winningAnswer3);
+			winCounter++;
+		} else {
+			$("#question").text("Nope!");
+			$("<p>" + solutions[2] + "</p>").appendTo("#question");
+			$("#question").append(winningAnswer3);
+			lossCounter++;
+		}
+	})
 }
 
+// ROUND FOUR 
 function compareClickValues4() {
-$("button").on("click", function() {
+  $("button").on("click", function() {
 	stop ();
-	if ($(this).index() == 12) {
-		$("#question").text("Correct!");
-		$("button").hide();
-		$("#question").append(winningAnswer4);
-		winCounter++;
-		setTimeout(roundFive, 1000 * 3);
-	} else {
-		$("#question").text("Nope!");
-		$("button").hide();
-		$("<p>" + solutions[3] + "</p>").appendTo("#question");
-		$("#question").append(winningAnswer4);
-		lossCounter++;
-		setTimeout(roundFive, 1000 * 3);
-	}
-  })
+	$("button").hide();
+	setTimeout(roundFive, 1000 * 3);
+		if ($(this).index() == 13) {
+			$("#question").text("Correct!");
+			$("<p>" + solutions[3] + "</p>").appendTo("#question");
+			$("#question").append(winningAnswer4);
+			winCounter++;
+		} else {
+			$("#question").text("Nope!");
+			$("<p>" + solutions[3] + "</p>").appendTo("#question");
+			$("#question").append(winningAnswer4);
+			lossCounter++;
+		}
+	})
 }
 
+// ROUND FIVE
 function compareClickValues5() {
-$("button").on("click", function() {
+  $("button").on("click", function() {
 	stop ();
-	if ($(this).index() == 16) {
-		$("#question").text("Correct!");
-		$("button").hide();
-		$("#question").append(winningAnswer5);
-		winCounter++;
-		setTimeout(roundFour, 1000 * 3);
-	} else {
-		$("#question").text("Nope!");
-		$("button").hide();
-		$("<p>" + solutions[4] + "</p>").appendTo("#question");
-		$("#question").append(winningAnswer5);
-		lossCounter++;
-		setTimeout(displayScore, 1000 * 3);
-	}
-  })
+	$("button").hide();
+	setTimeout(displayScore, 1000 * 3);
+		if ($(this).index() == 19) {
+			$("#question").text("Correct!");
+			$("<p>" + solutions[4] + "</p>").appendTo("#question");
+			$("#question").append(winningAnswer5);
+			winCounter++;
+		} else {
+			$("#question").text("Nope!");
+			$("<p>" + solutions[4] + "</p>").appendTo("#question");
+			$("#question").append(winningAnswer5);
+			lossCounter++;
+		}
+	})
 }
 
-// Step four: Stop timer when user clicks on any "choices" button x
+// GAME IS OVER
+function displayScore () {
+	$("#question").text("You made it! Here's how you did:")
+	$("#wins").text("Correct answers: " + winCounter)
+	$("#losses").text("Incorrect answers: " + lossCounter)
+	$("#unanswered").text("Unanswered: " + unanswered)
+	$("<button>" + "Start Over?" + "</button>").appendTo("#restart");
+	$("button").on("click", function() {
+		$("#wins").empty();
+		$("#losses").empty();
+		$("#unanswered").empty();
+		$("#restart").empty();
+		roundOne();
+	})
+}
 
-// Set timer to 30 seconds
+
+// TIMER: set countdown amount
 var timer = 20;
 // Variable to hold interval ID
 var intervalID;
 
-// The start function sets the clock and decrements by 1 per second
+// Set the clock and decrement by 1 per second
 function startTimer(){
-	timer = 10;
+	timer = 5;
 	$("#timer").text("Time remaining:" + (" ") + timer + (" ") + "seconds");
 	intervalID = setInterval(decrement, 1000);
 }
 
-// WORKING CODE DO NOT EDIT
 // Decrement function
 function decrement() {
 	timer--;
-
-// Replace timer with this one that is counting down
 	$("#timer").text("Time remaining:" + (" ") + timer + (" ") + "seconds");
 	if (timer === 0) {
 		stop();
-		$("#question").text("Time's up!!!");
 		$("button").hide();
+		$("#question").text("Time's up!!!");
 		$("<p>" + solutions[0] + "</p>").appendTo("#question");
 		$("#question").append(winningAnswer);
 		unanswered++;
 		setTimeout(roundTwo, 1000 * 3);
 	}
 }
-// END WORKING CODE BLOCK
 
 // The stop timer function
 function stop() {
 	clearInterval(intervalID);
 }
+// Adding in button hover elements
+$("button").hover(function(){
+    $(this).css("background-color", "#18c8e4");
+    }, function(){
+    $(this).css("background-color", "white");
+ });
 
-
+ // And some fun link hover decoration
+$("a").hover(function(){
+    $(this).css("background-color", "#18c8e4");
+     }, function(){
+    $(this).css("background-color", "white");
+ });
 
 
 
